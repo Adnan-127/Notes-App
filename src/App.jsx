81d3bc38086071e9ae36a986c6d83e13395e0e9c
Adnan-127 <adnan.github127@gmail.com>
2024-11-24 14:06:45 +0300

    update (App.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import NoteList from './components/NoteList';
-import AddNote from './components/AddNote';
-import UpdateNote from './components/UpdateNote';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import NoteList from "./components/NoteList";
+import AddNote from "./components/AddNote";
+import UpdateNote from "./components/UpdateNote";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="container">
-        <h1 className='text-danger text-center mt-5 mb-5'>Notes</h1>
+        <h1 className="text-danger text-center mt-5 mb-5">Notes</h1>
         <Routes>
-          <Route path='/' element={<NoteList />} />
-          <Route path='/add' element={<AddNote />} />
-          <Route path='/update/:index' element={<UpdateNote />} />
+          <Route path="/" element={<NoteList />} />
+          <Route path="/add" element={<AddNote />} />
+          <Route path="/update/:index" element={<UpdateNote />} />
         </Routes>
       </div>
     </Router>
